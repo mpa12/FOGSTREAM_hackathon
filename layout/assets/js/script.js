b27fda58503ce61aac452a4736d1490e947c1c30
mpa12 <maksim.28.05@mail.ru>
2022-05-13 17:22:33 +1000

    Код для свайпера в разделе Партнеры хакатона

--- layout/assets/js/script.js
+++ layout/assets/js/script.js
@@ -6,3 +6,39 @@ function open_menu() {
 		menu.style.display = 'flex';
 	}
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+	let partners = document.getElementById('partners');
+	let partners_child = document.getElementById('partners_child');
+
+	let mouse_down = false;
+	let first_px = 0;
+	let now_px = 0;
+	let difference_px = 0;
+	let partners_marginLeft = 0;
+	let change_px = 0;
+	let min_margin = 0;
+
+	partners.addEventListener("mousedown", (e) => {
+		mouse_down = true;
+		event.preventDefault();
+		first_px = e.pageX;
+		partners_marginLeft = parseFloat(partners_child.style.marginLeft);
+		min_margin = (partners_child.scrollWidth - document.documentElement.clientWidth) * -1;
+	})
+
+	partners.addEventListener("mouseup", () => {
+		mouse_down = false;
+	})
+
+	partners.addEventListener("mousemove", (e) => {
+		if (mouse_down) {
+			now_px = e.pageX;
+			difference_px = (first_px - now_px) * (-1);
+			change_px = partners_marginLeft + difference_px;
+			if (change_px <= 0 && change_px >= min_margin) {
+				partners_child.style.marginLeft = change_px + 'px';
+			}
+		}
+	})
+});
