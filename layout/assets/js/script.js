9a5b44e4f490a951e306b1f9ad4918ce0e563e5e
mpa12 <maksim.28.05@mail.ru>
2022-05-13 17:41:48 +1000

    Свайпер под телефоны

--- layout/assets/js/script.js
+++ layout/assets/js/script.js
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	let change_px = 0;
 	let min_margin = 0;
 
+	let down_list = ["mousedown", "touchstart"];
 	partners.addEventListener("mousedown", (e) => {
 		mouse_down = true;
 		event.preventDefault();
@@ -33,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	partners.addEventListener("mousemove", (e) => {
 		if (mouse_down) {
-			now_px = e.pageX;
+			now_px = e.targetTouches.screenX;
 			difference_px = (first_px - now_px) * (-1);
 			change_px = partners_marginLeft + difference_px;
 			if (change_px <= 0 && change_px >= min_margin) {
@@ -41,4 +42,29 @@ document.addEventListener("DOMContentLoaded", () => {
 			}
 		}
 	})
+
+
+	partners.addEventListener("touchstart", function(e) { 
+		mouse_down = true;
+		event.preventDefault();
+		first_px = e.targetTouches[0]['clientX'];
+		partners_marginLeft = parseFloat(partners_child.style.marginLeft);
+		min_margin = (partners_child.scrollWidth - document.documentElement.clientWidth) * -1;
+	}, true);
+
+	partners.addEventListener("touchend", function(e) {
+		mouse_down = false;
+	}, true)
+
+	partners.addEventListener("touchmove", function(e) {
+		if (mouse_down) {
+			now_px = e.targetTouches[0]['clientX'];
+			difference_px = (first_px - now_px) * (-1);
+			change_px = partners_marginLeft + difference_px;
+			if (change_px <= 0 && change_px >= min_margin) {
+				partners_child.style.marginLeft = change_px + 'px';
+			}
+		}
+		console.log(now_px)
+	}, true)
 });
