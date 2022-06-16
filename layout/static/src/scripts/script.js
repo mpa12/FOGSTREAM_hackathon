d850f0fa8e670baf854874c2ca587c68078dcac2
mpa12 <maksim.28.05@mail.ru>
2022-06-16 15:41:20 +1000

    case optiacs

--- layout/static/src/scripts/script.js
+++ layout/static/src/scripts/script.js
@@ -20,56 +20,60 @@ document.addEventListener("DOMContentLoaded", () => {
 	let change_px = 0;
 	let min_margin = 0;
 
-	partners.addEventListener("mousedown", (e) => {
-		mouse_down = true;
-		event.preventDefault();
-		first_px = e.pageX;
-		partners_marginLeft = parseFloat(partners_child.style.marginLeft);
-		min_margin = (partners_child.scrollWidth - document.documentElement.clientWidth) * -1;
-	})
-
-	partners.addEventListener("mouseup", () => {
-		mouse_down = false;
-	})
-
-	partners.addEventListener("mousemove", (e) => {
-		if (mouse_down) {
-			now_px = e.pageX;
-			difference_px = (first_px - now_px) * (-1);
-			change_px = partners_marginLeft + difference_px;
-			if (change_px <= 0 && change_px >= min_margin) {
-				partners_child.style.marginLeft = change_px + 'px';
+	try {
+		partners.addEventListener("mousedown", (e) => {
+			mouse_down = true;
+			event.preventDefault();
+			first_px = e.pageX;
+			partners_marginLeft = parseFloat(partners_child.style.marginLeft);
+			min_margin = (partners_child.scrollWidth - document.documentElement.clientWidth) * -1;
+		})
+	
+		partners.addEventListener("mouseup", () => {
+			mouse_down = false;
+		})
+	
+		partners.addEventListener("mousemove", (e) => {
+			if (mouse_down) {
+				now_px = e.pageX;
+				difference_px = (first_px - now_px) * (-1);
+				change_px = partners_marginLeft + difference_px;
+				if (change_px <= 0 && change_px >= min_margin) {
+					partners_child.style.marginLeft = change_px + 'px';
+				}
 			}
-		}
-	})
-
-	partners.addEventListener("touchstart", function(e) { 
-		mouse_down = true;
-		event.preventDefault();
-		first_px = e.targetTouches[0]['clientX'];
-		partners_marginLeft = parseFloat(partners_child.style.marginLeft);
-		min_margin = (partners_child.scrollWidth - document.documentElement.clientWidth) * -1;
-	}, true);
-
-	partners.addEventListener("touchend", function(e) {
-		mouse_down = false;
-	}, true)
-
-	partners.addEventListener("touchmove", function(e) {
-		if (mouse_down) {
-			now_px = e.targetTouches[0]['clientX'];
-			difference_px = (first_px - now_px) * (-1);
-			change_px = partners_marginLeft + difference_px;
-			if (change_px <= 0 && change_px >= min_margin) {
-				partners_child.style.marginLeft = change_px + 'px';
+		})
+	
+		partners.addEventListener("touchstart", function(e) { 
+			mouse_down = true;
+			event.preventDefault();
+			first_px = e.targetTouches[0]['clientX'];
+			partners_marginLeft = parseFloat(partners_child.style.marginLeft);
+			min_margin = (partners_child.scrollWidth - document.documentElement.clientWidth) * -1;
+		}, true);
+	
+		partners.addEventListener("touchend", function(e) {
+			mouse_down = false;
+		}, true)
+	
+		partners.addEventListener("touchmove", function(e) {
+			if (mouse_down) {
+				now_px = e.targetTouches[0]['clientX'];
+				difference_px = (first_px - now_px) * (-1);
+				change_px = partners_marginLeft + difference_px;
+				if (change_px <= 0 && change_px >= min_margin) {
+					partners_child.style.marginLeft = change_px + 'px';
+				}
 			}
-		}
-	}, true)
-
-	window.addEventListener('resize', function(event) {
-		partners_child.style.marginLeft = '0px';
-		window_height = window.innerHeight;
-	}, true);
+		}, true)
+	
+		window.addEventListener('resize', function(event) {
+			partners_child.style.marginLeft = '0px';
+			window_height = window.innerHeight;
+		}, true);
+	} catch {
+		console.log('ERROR')
+	}
 
 	// animate
 	window.addEventListener('scroll', function(event) {
